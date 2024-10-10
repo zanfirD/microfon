addd5fc51bab4f7f0b09af8a90b6d00aa6059a94
zanfirD <159519161+zanfirD@users.noreply.github.com>
2024-10-10 09:06:41 +0300

    Add files via upload

---
+++ speech.js
@@ -0,0 +1,26 @@
+document.addEventListener("touchstart", on_touch);
+document.addEventListener("mousedown", on_touch);
+
+var recognition = new webkitSpeechRecognition();
+recognition.lang = 'en-US';
+
+function on_touch() {
+    if (!recognition_started) {
+        recognition.start();
+        recognition_started = true;
+    }
+}
+
+function onend() {
+    recognition.stop();
+    recognition_started = false;
+}
+
+recognition.onend = onend;
+recognition.onsoundend = onend;
+recognition.onspeechend = onend;
+recognition.onresult = on_results;
+
+function on_results(e) {
+    document.getElementById("text").innerHTML += "Ați rostit cuvântul: " + e.results[0][0].transcript + ", acuratețe: " + e.results[0][0].confidence + "<br>";
+}
