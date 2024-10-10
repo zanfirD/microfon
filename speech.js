3909cce3cda586fa89c905426aec3f2c55b0b756
zanfirD <159519161+zanfirD@users.noreply.github.com>
2024-10-10 10:12:33 +0300

    Update speech.js

--- speech.js
+++ speech.js
@@ -2,10 +2,12 @@ document.addEventListener("touchstart", on_touch);
 document.addEventListener("mousedown", on_touch);
 
 var recognition = new webkitSpeechRecognition();
+var recognition_started = false;  // Variabilă globală pentru a verifica dacă recunoașterea a început
+
 recognition.lang = 'en-US';
 
 function on_touch() {
-    if (!recognition_started) {
+    if (!recognition_started) {  // Verificăm dacă recunoașterea nu a început deja
         recognition.start();
         recognition_started = true;
     }
@@ -17,10 +19,10 @@ function onend() {
 }
 
 recognition.onend = onend;
-recognition.onsoundend = onend;
 recognition.onspeechend = onend;
-recognition.onresult = on_results;
+recognition.onresult = on_results;  // Corectarea numelui evenimentului
 
 function on_results(e) {
-    document.getElementById("text").innerHTML += "Ați rostit cuvântul: " + e.results[0][0].transcript + ", acuratețe: " + e.results[0][0].confidence + "<br>";
+    document.getElementById("text").innerHTML += "Ati rostit cuvantul: " +
+        e.results[0][0].transcript + ", acuratete: " + e.results[0][0].confidence + "<br>";
 }
