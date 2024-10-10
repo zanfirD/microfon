7af0d3eb33023a068a8ee0d3b2f717c7b9a965ca
zanfirD <159519161+zanfirD@users.noreply.github.com>
2024-10-10 09:13:26 +0300

    Update speech.js

--- speech.js
+++ speech.js
@@ -4,6 +4,8 @@ document.addEventListener("mousedown", on_touch);
 var recognition = new webkitSpeechRecognition();
 recognition.lang = 'en-US';
 
+var recognition_started = false;
+
 function on_touch() {
     if (!recognition_started) {
         recognition.start();
@@ -19,8 +21,14 @@ function onend() {
 recognition.onend = onend;
 recognition.onsoundend = onend;
 recognition.onspeechend = onend;
+
 recognition.onresult = on_results;
 
 function on_results(e) {
     document.getElementById("text").innerHTML += "Ați rostit cuvântul: " + e.results[0][0].transcript + ", acuratețe: " + e.results[0][0].confidence + "<br>";
+}
+
+recognition.onerror = function(event) {
+    console.log('Eroare la recunoașterea vocală: ' + event.error);
+    alert('Eroare la recunoașterea vocală: ' + event.error);
 }
